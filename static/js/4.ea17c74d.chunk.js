69418008b56efe58d25083a06cf04d466de9cf42
drac94 <info@luisguerrero.me>
2022-03-23 17:42:30 -0600

    Updates

--- static/js/4.ea17c74d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpminimal-react-template"]=this["webpackJsonpminimal-react-template"]||[]).push([[4],{19:function(e,t,n){},23:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r}));n(0),n(19);var c=n(3),r=function(){return Object(c.jsx)("div",{className:"french-press-container",children:Object(c.jsx)("h1",{children:"French Press Calculator"})})}}}]);
-//# sourceMappingURL=4.ea17c74d.chunk.js.map
